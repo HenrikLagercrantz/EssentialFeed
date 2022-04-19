578f66da1c3165e7745c5f40f78c39ad37930283
Henrik Lagercrantz <info@henriklagercrantz.se>
2022-04-19 19:04:39 +0200

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Henrik Lagercrantz on 2022-04-19.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
