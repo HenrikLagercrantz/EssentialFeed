35bb9eaa0a2d5a865a1fcbae3d5c98a0dbfb9f49
Henrik Lagercrantz <info@zelo.se>
2022-04-20 17:51:35 +0200

    Request data from URl on load

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,14 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
     var requestedURL: URL?
 }
 
@@ -23,6 +27,16 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL(){
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+        
     }
 
 }
